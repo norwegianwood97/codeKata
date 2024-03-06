918b516fbc3946a3cc217c27cf1918df48869c88
norwegianwood97 <118883093+norwegianwood97@users.noreply.github.com>
2024-03-06 14:28:13 +0900

    [level 1] Title: 대충 만든 자판, Time: 0.70 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
@@ -1,33 +1,37 @@
 function solution(keymap, targets) {
     var answer = [];
-    // 문자와 그 문자를 생성하기 위한 키 입력 정보를 저장하는 맵
-    let charToKeyPress = {};
+    let charPosition = {};
 
-    // keymap을 순회하며 charToKeyPress를 채움
-    for (let keyIndex = 0; keyIndex < keymap.length; keyIndex++) {
-        const keyChars = keymap[keyIndex];
-        for (let charIndex = 0; charIndex < keyChars.length; charIndex++) {
-            const char = keyChars[charIndex];
-            // 같은 문자가 여러 키에 할당되었거나 한 키에 여러 번 할당된 경우, 최소 횟수를 저장
-            if (!charToKeyPress[char] || charToKeyPress[char] > charIndex + 1) {
-                charToKeyPress[char] = charIndex + 1;
+    // keymap	            targets	        result
+    // ["ABACD", "BCEFD"]	["ABCD","AABB"]	[9, 4]
+    // ["AA"]	            ["B"]	        [-1]
+    // ["AGZ", "BSSS"]	    ["ASA","BGZ"]	[4, 6]
+
+
+    // keymap으로 charPosition 먼저 만들기!
+    for(let keyIndex = 0; keyIndex<keymap.length; keyIndex++){
+        let keyString = keymap[keyIndex];
+        for(let charIndex = 0; charIndex<keyString.length;charIndex++){
+            let char = keyString[charIndex];
+            if(!charPosition[char] || charPosition[char] > charIndex+1){
+                charPosition[char] = charIndex + 1;
             }
         }
-    }
+    }   // keymap["ABACD", "BCEFD"] 에 대해 
+        // charPosition={'A' : 1, 'B' : 1, 'C' : 2, 'D' : 5, 'E' : 3, 'F' : 4}
 
-    // targets 배열을 순회하며 각 문자열을 생성하기 위한 최소 키 입력 횟수를 계산
-    targets.forEach(target => {
-        let keyPressCount = 0;
-        for (let char of target) {
-            if (!charToKeyPress[char]) {
-                // 만약 특정 문자를 생성할 수 있는 키가 없다면 -1 저장 후 반복 종료
-                keyPressCount = -1;
+    // targets으로 answer 만들기
+    for(let temp_targets of targets){
+        let temp_answer = 0;
+        for(let tempChar of temp_targets){
+            if(!charPosition[tempChar]){
+                temp_answer = -1;
                 break;
             }
-            keyPressCount += charToKeyPress[char];
+            temp_answer += charPosition[tempChar];
         }
-        answer.push(keyPressCount);
-    });
+        answer.push(temp_answer);
+    }
 
     return answer;
 }
